f293b1e97be88e025518c397f69b5078a5538866
Ebad-ur-Rehman <ebadinfalltraders@gmail.com>
2024-08-23 19:36:43 +0500

    pawn hints added

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -16,7 +16,12 @@ export default function Board() {
         '6 1': {'url': './images/pawn_w.png', 'name': 'pawn_w', 'i': 6, 'j': 1},
         '7 1': {'url': './images/pawn_w.png', 'name': 'pawn_w', 'i': 7, 'j': 1},
         
-        '4 4': {'url': './images/rook_w.png', 'name': 'rook_w', 'i': 4, 'j': 4},
+        '1 2': {'url': './images/rook_w.png', 'name': 'rook_w', 'i': 1, 'j': 2},
+        '3 2': {'url': './images/rook_b.png', 'name': 'rook_b', 'i': 3, 'j': 2},
+        '5 3': {'url': './images/rook_b.png', 'name': 'rook_b', 'i': 5, 'j': 3},
+        '7 3': {'url': './images/rook_b.png', 'name': 'rook_b', 'i': 7, 'j': 3},
+        '3 3': {'url': './images/rook_w.png', 'name': 'rook_w', 'i': 3, 'j': 3},
+
 
         '0 0': {'url': './images/rook_w.png', 'name': 'rook_w', 'i': 0, 'j': 0},
         '1 0': {'url': './images/knight_w.png', 'name': 'knight_w', 'i': 1, 'j': 0},
--- src/Components/HighlightHintsData.jsx
+++ src/Components/HighlightHintsData.jsx
@@ -1,11 +1,36 @@
 export function highlightHint(i, j, isPieceExists, updateHighlightHints, boardState) {
     if(isPieceExists['name'] == 'pawn_w'){
         updateHighlightHints(prevHighlightHints => {
-            return {
-                [`${i} ${j+1}`]: 'hint',
-                [`${i} ${j+2}`]: 'hint',
+            let hints = {}
+            if(boardState[`${i} ${j+1}`]) {
+                // rival piece diagonal
+                if(boardState[`${i} ${j+1}`].name.endsWith('_b') || boardState[`${i} ${j+1}`].name.endsWith('_w')) {
+                    
+                }
+            }
+            else {
+                hints[`${i} ${j+1}`] = 'hint'
+            }
 
-        }})            
+            // check if diagonal right element have enemy or fellow
+            if(boardState[`${i+1} ${j+1}`]){
+                boardState[`${i+1} ${j+1}`].name.endsWith('_b')?hints[`${i+1} ${j+1}`] = 'danger':null
+            }
+            
+
+            // check if diagonal left element have enemy or fellow
+            if(boardState[`${i-1} ${j+1}`]){
+                boardState[`${i-1} ${j+1}`].name.endsWith('_b')?hints[`${i-1} ${j+1}`] = 'danger':null
+            }
+
+            // 2nd place element
+            if(boardState[`${i} ${j+2}`] || boardState[`${i} ${j+1}`]) {
+            }
+            else {
+                hints[`${i} ${j+2}`] = 'hint'
+            }
+            return hints
+        })            
     }
     else if(isPieceExists['name'] == 'rook_w'){
         updateHighlightHints(prevHighlightHints => {
@@ -13,7 +38,7 @@ export function highlightHint(i, j, isPieceExists, updateHighlightHints, boardSt
             // upper ones
             for(let u=j+1; u<=7; u++) {
             if(boardState[`${i} ${u}`]) {
-                    boardState[`${i} ${u}`].name.endsWith('_w')?null:hints[`${i} ${u}`] = 'advantage'
+                    boardState[`${i} ${u}`].name.endsWith('_w')?null:hints[`${i} ${u}`] = 'danger'
                     break
             }
             else {
@@ -23,7 +48,7 @@ export function highlightHint(i, j, isPieceExists, updateHighlightHints, boardSt
 
         for(let l=j-1; l>=0; l--) {
             if(boardState[`${i} ${l}`]) {
-                    boardState[`${i} ${l}`].name.endsWith('_w')?null:hints[`${i} ${l}`] = 'advantage'
+                    boardState[`${i} ${l}`].name.endsWith('_w')?null:hints[`${i} ${l}`] = 'danger'
                     break
             }
             else {
@@ -33,7 +58,7 @@ export function highlightHint(i, j, isPieceExists, updateHighlightHints, boardSt
 
         for(let lf=i-1; lf>=0; lf--) {
             if(boardState[`${lf} ${j}`]) {
-                    boardState[`${lf} ${j}`].name.endsWith('_w')?null:hints[`${lf} ${j}`] = 'advantage'
+                    boardState[`${lf} ${j}`].name.endsWith('_w')?null:hints[`${lf} ${j}`] = 'danger'
                     break
             }
             else {
@@ -43,7 +68,7 @@ export function highlightHint(i, j, isPieceExists, updateHighlightHints, boardSt
 
         for(let r=i+1; r<=7; r++) {
             if(boardState[`${r} ${j}`]) {
-                boardState[`${r} ${j}`].name.endsWith('_w')?null:hints[`${r} ${j}`] = 'advantage'
+                boardState[`${r} ${j}`].name.endsWith('_w')?null:hints[`${r} ${j}`] = 'danger'
                 break
             }
             else {
