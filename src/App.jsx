8807c45b985e48c32abda7891c42342d69cc7475
Ebad-ur-Rehman <Ebad-ur-Rehman@mx>
2024-09-11 16:27:51 +0500

    continue on linux

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Board from './Components/Board'
 import React from 'react'
-import { TilePawnSelection } from './Components/tile'
+import { TilePawnSelection } from './Components/Tile'
 
 
 function App() {
